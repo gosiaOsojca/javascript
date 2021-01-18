2a7b2f5c5099f6d2bedc4b17559d2d590b4f5854
gosia.pulawska <pulawska.malgosia@gmail.com>
2021-01-18 17:50:03 +0100

    Create script.js

---
+++ task#3/script.js
@@ -0,0 +1,31 @@
+// TASK #3
+
+var setZeroes = function (matrix) {
+    let i;
+    let j;
+    let dataTable = [];
+    alert(matrix[0].length);
+    for (i = 0; i < matrix.length; i++) {
+        for (j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                let element = {
+                    row: i,
+                    column: j
+                };
+                dataTable.push(element);
+            }
+        }
+    }
+    let k;
+    for (k = 0; k < dataTable.length; k++) {
+        matrix[dataTable[k].row].fill(0);
+        for (i = 0; i < matrix.length; i++) {
+            for (j = 0; j < matrix[i].length; j++) {
+                if (j === dataTable[k].column) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    };
+}
+// END TASK #3
