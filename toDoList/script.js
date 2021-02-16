985627c047f1e4b89c71551cd06bb37d0e758d47
gosia.pulawska <pulawska.malgosia@gmail.com>
2021-02-16 19:06:11 +0100

    save to local storage function

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,9 +1,12 @@
 function addItem() {
   let listItem = document.createElement('li');
   listItem.className = 'list__item';
+  let itemSpan = document.createElement('SPAN');
+  itemSpan.className = 'list__item-span'
   let inputValue = document.getElementById('input').value;
   let textNode = document.createTextNode(inputValue);
-  listItem.appendChild(textNode);
+  itemSpan.appendChild(textNode);
+  listItem.appendChild(itemSpan);
 
   inputValue ? document.getElementById('list').appendChild(listItem) : alert("Fill the field");
 
@@ -19,9 +22,24 @@ function addItem() {
   for (button of closeButtons) {
     button.addEventListener('click', function () {
       this.parentElement.style.display = 'none';
+      if (saveButton.classList.contains('save-button--clicked')) {
+        localStorage.removeItem();
+        storageItemIndex--;
+      }
     }, false);
   }
+
+  if(saveButton.classList.contains('save-button--clicked')) {
+    localStorage.setItem(`item${storageItemIndex++}`, inputValue);
+  }
 }
+
+function changeButton() {
+  this.classList.toggle('save-button--clicked');
+}
+
+
+let storageItemIndex = 0;
 
 let addButton = document.getElementById('addButton');
 addButton.addEventListener('click', addItem, false);
@@ -32,3 +50,20 @@ list.addEventListener('click', function (e) {
     e.target.classList.toggle('list__item--checked');
   }
 }, false);
+
+let saveButton = document.getElementById('saveButton');
+saveButton.addEventListener('click', changeButton, false);
+
+
+saveButton.addEventListener('click', function () {
+  if (this.classList.contains('save-button--clicked')) {
+    let listItems = document.getElementsByClassName('list__item-span');
+    for (item of listItems) {
+      localStorage.setItem(`item${storageItemIndex++}`, item.textContent);
+    }
+  } else {
+    localStorage.clear();
+    storageItemIndex = 0;
+  }
+}, false);
+
